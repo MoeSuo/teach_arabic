2dc6a49e545b29bcb7c1bb4edee334c2d9dfd807
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-10-17 11:45:27 +0300

    Add style to profile page

--- src/pages/user/profile.js
+++ src/pages/user/profile.js
@@ -21,7 +21,9 @@ function Profile() {
           <h1 className="text-4xl font-bold uppercase">User Profile</h1>
           <div>
             <br /> <br /> <br /> <br />
-            <h4 className="text-4xl font-bold uppercase text-center">Welcome:</h4>
+            <h4 className="text-4xl font-bold uppercase text-center">
+              Welcome:
+            </h4>
             <br />
             <h4 className="text-4xl font-light uppercase justify-center">
               {session.user.email}
@@ -37,16 +39,18 @@ function Profile() {
         </div>
       ) : (
         // User is not logged in, display the notLoggedIn div
-        <div id="notLoggedIn" className="flex items-center justify-center h-screen flex-col mx-36 text-center">
-          <h3 className="text-4xl font-light uppercase justify-center">
-            You need to log in first to see your profile page
-          </h3>
+        <div
+          id="notLoggedIn"
+          className="flex items-center justify-center h-screen flex-col mx-14 text-center"
+        >
+          <h3 >You need to </h3>{" "}
+          <Link href={"/user/login"} className="uppercase font-bold text-[1.7rem] ">log in</Link>{" "}
+          <h3>to see your profile page </h3>
           <Link
-          href={"/user/login"}
-          className="bg-blue-500 content-center my-8 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-
+            href={"/user/login"}
+            className="bg-blue-500 content-center my-8 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-          Login
+            Login
           </Link>
         </div>
       )}
